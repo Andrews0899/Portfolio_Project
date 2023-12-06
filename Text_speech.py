40768594e58daf5161c52dae08285dba8cb6f5ca
Andrews0899 <andrewsmakafui350@gmail.com>
2023-12-06 01:34:13 +0000

    updated the images

--- Text_speech.py
+++ Text_speech.py
@@ -75,21 +75,21 @@ def download():
 
 
 #icon
-image_icon=PhotoImage(file="speaker.png")
+image_icon=PhotoImage(file="speak.png")
 root.iconphoto(False,image_icon)
 
 #Top Frame
 Top_frame=Frame(root,bg="white",width=900,height=100)
 Top_frame.place(x=0,y=0)
 
-Logo=PhotoImage(file="speaker logo.png")
+Logo=PhotoImage(file="speak logo.png")
 Label(Top_frame,image=Logo,bg="white").place(x=10,y=5)
 
 Label(Top_frame,text="TEXT TO SPEECH",font="arial 20 bold",bg="white",fg="black").place(x=100,y=30)
 
 
 #########
-text_area=Text(root,font="Robote 20",bg="white",relief=GROOVE,wrap=WORD)
+text_area=Text(root,font="Roboto 20",bg="white",relief=GROOVE,wrap=WORD)
 text_area.place(x=10,y=150,width=500,height=250)
 
 Label(root,text="VOICE",font="arial 15 bold",bg="#305065",fg="white").place(x=580,y=160)
@@ -103,7 +103,7 @@ speed_combobox=Combobox(root,values=['Fast','Normal','Slow'],font="arial 14",sta
 speed_combobox.place(x=730,y=200)
 speed_combobox.set('Normal')
 
-imageicon=PhotoImage(file="speaker.png")
+imageicon=PhotoImage(file="speak.png")
 btn=Button(root,text="Speak",compound=LEFT,image=imageicon,width=130,font="arial 14 bold",command=speaknow)
 btn.place(x=550,y=280)
 
